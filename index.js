f51d59b85c75ce47be273b9dfc4e8beaf29ce6a2
replacem3nts <donny.landis@gmail.com>
2020-04-26 22:41:10 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(history) {
+  let yearRecord = history.find((y) => {
+    return y['result'] === "W"
+  })
+  if (yearRecord) {return yearRecord["year"]}
+}
